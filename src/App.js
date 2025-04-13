e34b678f07c5e4c6923fcb8360b7d123a1f50288
Anurag Baranwal <anuragbaranwal789@gmail.com>
2025-04-13 22:28:11 +0530

    All package installed, Pages updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-export default function App() {
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Loader from "./components/Loader";
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <div><Navbar/></div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+      </Routes>
+    </div>
   )
 }
+
+export default App;
